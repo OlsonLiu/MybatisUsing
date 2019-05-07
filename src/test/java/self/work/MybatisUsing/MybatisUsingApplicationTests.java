8fde7df885453de00ca7c036a3a95d9b7315c276
OlsonLiu <satis.iim03g@g2.nctu.edu.tw>
2019-05-07 23:36:33 +0800

    app test

--- src/test/java/self/work/MybatisUsing/MybatisUsingApplicationTests.java
+++ src/test/java/self/work/MybatisUsing/MybatisUsingApplicationTests.java
@@ -4,9 +4,10 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import self.work.app.MybatisUsingApplication;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes = MybatisUsingApplication.class)
 public class MybatisUsingApplicationTests {
 
 	@Test
